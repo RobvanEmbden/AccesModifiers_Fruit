4dea83845ccefede4c8d02297a9c7e3509c16bec
RobvanEmbden <61789232+RobvanEmbden@users.noreply.github.com>
2021-03-07 17:46:01 +0100

    Add files via upload

---
+++ retailAccesModifiersFruit/retailAccesModifiersFruit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace retailAccesModifiersFruit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Coords c1 = new Coords();
+            c1.x = 100;
+            c1.y = 500;
+            Coords c2 = c1;
+            Console.WriteLine(c2.x + c2.y);
+            // public - acces isn't restricted
+            // private acces is restricted to the containing class (local)
+            // proteced - acces is restricted to the containing class
+            // internal - acces is restricted to code in the same assembly (dll or exe)
+            // protected internal - combination of proteced and internal
+        }
+    }
+    public struct Coords // became public as private won't run, which has to be within the class above.
+    {
+        public double x, y;
+    }
+}
